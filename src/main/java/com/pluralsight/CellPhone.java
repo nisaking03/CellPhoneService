cde7c6cd5e3af5959779f408e3bacc0986102a1e
nisaking03 <nisa.king03@gmail.com>
2025-10-02 13:39:01 -0400

    All changes

--- src/main/java/com/pluralsight/CellPhone.java
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -1,4 +1,69 @@
 package com.pluralsight;
 
 public class CellPhone {
+    private int serialNumber;
+    private String model;
+    private String carrier;
+    private String phoneNumber;
+    private String owner;
+
+    public CellPhone(){
+        this.serialNumber = 0;
+        this.model = "";
+        this.carrier = "";
+        this.phoneNumber = "";
+        this.owner = "";
+    }
+
+    public int getSerialNumber(){
+        return this.serialNumber;
+    }
+
+    public void setSerialNumber (int newSerialNumber){
+        this.serialNumber = newSerialNumber;
+    }
+
+    public String getModel(){
+        return this.model;
+    }
+
+    public void setModel(String model){
+        this.model = model;
+    }
+
+    public String getCarrier(){
+        return this.carrier;
+    }
+
+    public void setCarrier(String carrier){
+        this.carrier = carrier;
+    }
+
+    public String getPhoneNumber(){
+        return this.phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getOwner(){
+        return this.owner;
+    }
+
+    public void setOwner(String owner){
+        this.owner = owner;
+    }
+
+    public void dial(String phoneNumberToCall){
+        System.out.println(owner + " " + "is calling..." + phoneNumberToCall);
+        System.out.println("Riiiiiiinnnnnggg!");
+        System.out.println("Ringgg!");
+        System.out.println("Ring....");
+    }
+
+    public void dial(CellPhone phone){
+        dial(phone.getPhoneNumber());
+    }
+
 }
--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -1,4 +1,57 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class CellPhoneApplication {
+    public static void main (String [] args){
+        Scanner sc = new Scanner(System.in);
+        CellPhone myPhone = new CellPhone();
+
+        System.out.print("What is the serial number?: ");
+        int sn = sc.nextInt();
+        sc.nextLine();
+
+        myPhone.setSerialNumber(sn);
+
+        //model
+        System.out.print("What is the model: ");
+        String model = sc.nextLine();
+
+        myPhone.setModel(model);
+
+        //carrier
+        System.out.print("What is the carrier: ");
+        String carrier = sc.nextLine();
+
+        myPhone.setCarrier(carrier);
+
+        //phone number
+        System.out.print("What is your phone number: ");
+        String phoneNumber = sc.nextLine();
+
+        myPhone.setPhoneNumber(phoneNumber);
+
+        //owner
+        System.out.print("Who is the owner: ");
+        String owner = sc.nextLine();
+
+        myPhone.setOwner(owner);
+        myPhone.dial("911");
+
+
+        display(myPhone);
+
+    }
+    private static void display(CellPhone phone){
+        System.out.println("-------------------------------");
+        System.out.println("CELL PHONE [SN: " + phone.getSerialNumber() + "]");
+        System.out.println("  Owner:    " + phone.getOwner());
+        System.out.println("  Carrier:  " + phone.getCarrier());
+        System.out.println("  Model:    " + phone.getModel());
+        System.out.println("  Number:   41" + phone.getPhoneNumber());
+    }
 }
+
+
+
+
